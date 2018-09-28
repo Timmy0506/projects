5d986db0436066cb5ae874a37db41d5353252c7e
Tim Spekking <43640883+Timmy0506@users.noreply.github.com>
2018-09-28 12:44:51 +0200

    naam

---
+++ java applet/src/Show.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Show extends Applet {
+
+        public void init() {
+            setBackground(Color.blue);
+
+    }
+    public void paint(Graphics g) {
+            g.setColor(Color.green);
+            g.drawString("Tim!!", 50, 60 );
+    }
+
+}
+
